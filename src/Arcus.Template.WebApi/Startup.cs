668d77481339f9889bd9e2e1d98866a760177277
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2019-09-11 08:05:00 +0200

    FEAT: add initial template proj with exception handling

---
+++ src/Arcus.Template.WebApi/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Arcus.Template.WebApi
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMiddleware<Arcus.WebApi.Logging.ExceptionHandlingMiddleware>();
+
+            #warning "Please configure application with HTTPS transport layer security"
+            #warning "Please configure application with authentication mechanism: https://webapi.arcus-azure.net/features/security/auth/shared-access-key"
+
+            app.UseMvc();
+        }
+    }
+}

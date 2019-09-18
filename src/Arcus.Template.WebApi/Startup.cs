c15a8423ce232e4f71c015202a2886a4ee345f15
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2019-09-18 10:25:28 +0200

    FEAT: only allow 'JsonInputFormatter'

--- src/Arcus.Template.WebApi/Startup.cs
+++ src/Arcus.Template.WebApi/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace Arcus.Template.WebApi
 {
@@ -10,7 +13,22 @@ namespace Arcus.Template.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => 
+            {
+                options.ReturnHttpNotAcceptable = true;
+                options.RespectBrowserAcceptHeader = true;
+                
+                RestrictToJsonContentType(options);
+            });
+        }
+
+        private static void RestrictToJsonContentType(MvcOptions options)
+        {
+            var allButJsonInputFormatters = options.InputFormatters.Where(formatter => !(formatter is JsonInputFormatter));
+            foreach (IInputFormatter inputFormatter in allButJsonInputFormatters)
+            {
+                options.InputFormatters.Remove(inputFormatter);
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

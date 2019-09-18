80e51fc4ef01da64af3af62825cd7e50e5ef9d1a
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2019-09-18 09:27:30 +0200

    FEAT: add health endpoint to default API project

---
+++ src/Arcus.Template.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using GuardNet;
+
+namespace Arcus.Template.WebApi.Controllers
+{
+    /// <summary>
+    /// API endpoint to check the health of the application.
+    /// </summary>
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly HealthCheckService _healthCheckService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HealthController"/> class.
+        /// </summary>
+        /// <param name="healthCheckService">The service to provide the health of the API application.</param>
+        public HealthController(HealthCheckService healthCheckService)
+        {
+            Guard.NotNull(healthCheckService, nameof(healthCheckService));
+
+            _healthCheckService = healthCheckService;
+        }
+
+        /// <summary>
+        ///     Get health of API application.
+        /// </summary>
+        /// <remarks>Provides an indication about the health of the API and its dependencies (Sim Database).</remarks>
+        /// <response code="200">API is healthy</response>
+        /// <response code="503">API is unhealthy or in degraded state</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(HealthReport), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(HealthReport), StatusCodes.Status503ServiceUnavailable)]
+        public async Task<IActionResult> Get()
+        {
+            HealthReport healthReport = await _healthCheckService.CheckHealthAsync();
+            
+            if (healthReport.Status == HealthStatus.Healthy)
+            {
+                return Ok(healthReport);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable, healthReport);
+            }
+        }
+    }
+}

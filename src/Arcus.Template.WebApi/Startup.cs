da331050fe6e5f2023b678e197f71b0f7e95f9e7
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2019-09-24 11:53:49 +0200

    FEAT: add SharedAccessKey as authentication option in the API tem… (#19)
    * FEAT: add SharedAccessKey as authentication option in the API template
    
    * PR-MERGE: 'master'
    
    * PR-SUG: remove unnecessary quotes in warning messages

--- src/Arcus.Template.WebApi/Startup.cs
+++ src/Arcus.Template.WebApi/Startup.cs
@@ -4,7 +4,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
+#if Auth
+using Arcus.Security.Secrets.Core.Caching;
+using Arcus.Security.Secrets.Core.Interfaces;
+#endif
+#if SharedAccessKeyAuth
+using Arcus.WebApi.Security.Authentication.SharedAccessKey;
+#endif
 
 namespace Arcus.Template.WebApi
 {
@@ -14,12 +20,22 @@ namespace Arcus.Template.WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+#if Auth
+            #warning Please provide a valid secret provider, for example Azure Key Vault: https://security.arcus-azure.net/features/secrets/consume-from-key-vault
+            services.AddScoped<ICachedSecretProvider>(serviceProvider => new CachedSecretProvider(secretProvider: null));
+#endif
+
             services.AddMvc(options => 
             {
                 options.ReturnHttpNotAcceptable = true;
                 options.RespectBrowserAcceptHeader = true;
                 
                 RestrictToJsonContentType(options);
+
+#if SharedAccessKeyAuth                
+                #warning Please provide a valid request header name and secret name to the shared access filter
+                options.Filters.Add(new SharedAccessKeyAuthenticationFilter("YOUR REQUEST HEADER NAME", "YOUR SECRET NAME"));
+#endif
             });
 
             services.AddHealthChecks();
@@ -39,9 +55,11 @@ namespace Arcus.Template.WebApi
         {
             app.UseMiddleware<Arcus.WebApi.Logging.ExceptionHandlingMiddleware>();
 
-            #warning "Please configure application with HTTPS transport layer security"
-            #warning "Please configure application with authentication mechanism: https://webapi.arcus-azure.net/features/security/auth/shared-access-key"
+            #warning Please configure application with HTTPS transport layer security
 
+#if NoneAuth
+            #warning Please configure application with authentication mechanism: https://webapi.arcus-azure.net/features/security/auth/shared-access-key
+#endif
             app.UseMvc();
         }
     }

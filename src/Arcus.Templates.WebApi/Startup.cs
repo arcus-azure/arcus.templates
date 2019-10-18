058b72850796da46330525d0fba8ec6dcba49eaa
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2019-10-18 08:37:46 +0200

    FIX: force auth config before running

--- src/Arcus.Templates.WebApi/Startup.cs
+++ src/Arcus.Templates.WebApi/Startup.cs
@@ -26,7 +26,8 @@ namespace Arcus.Templates.WebApi
         {
 #if Auth
             #warning Please provide a valid secret provider, for example Azure Key Vault: https://security.arcus-azure.net/features/secrets/consume-from-key-vault
-            services.AddScoped<ICachedSecretProvider>(serviceProvider => new CachedSecretProvider(secretProvider: null));
+            var cachedSecretProvider = new CachedSecretProvider(secretProvider: null);
+            services.AddScoped<ICachedSecretProvider>(serviceProvider => cachedSecretProvider);
 #endif
 
             services.AddMvc(options => 

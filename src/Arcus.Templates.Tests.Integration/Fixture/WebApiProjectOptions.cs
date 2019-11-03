8895d4203740af92ed7bf04653e3278717c42ce5
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2019-11-03 11:47:21 +0100

    PR-SUG: remove dev appsettins

--- src/Arcus.Templates.Tests.Integration/Fixture/WebApiProjectOptions.cs
+++ src/Arcus.Templates.Tests.Integration/Fixture/WebApiProjectOptions.cs
@@ -142,15 +142,10 @@ namespace Arcus.Templates.Tests.Integration.Fixture
 
         private static void ConfigureCertificateSubjectAuthentication(DirectoryInfo projectDirectory, string subject)
         {
-            //ReplaceProjectFileContent(
-            //    projectDirectory,
-            //    "appsettings.Development.json",
-            //    appSettingsContent => appSettingsContent.Replace("YOUR KEY TO CERTIFICATE SUBJECT NAME", subject));
-
             ReplaceProjectFileContent(
                 projectDirectory,
                 "appsettings.json",
-                appSettingsContent => appSettingsContent.Replace("#{CertificateSubject}#", subject));
+                appSettingsContent => appSettingsContent.Replace("YOUR CERTIFICATE SUBJECT NAME", subject));
         }
 
         private static void ReplaceProjectFileContent(DirectoryInfo projectDirectory, string fileName, Func<string, string> replacements)

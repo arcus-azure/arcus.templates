e7fa6dc4e0ec84b8d1c1567c8aa30ce7b17307e4
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-01-29 12:26:05 +0100

    Update src/Arcus.Templates.WebApi/Program.cs
    Co-Authored-By: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Arcus.Templates.WebApi/Program.cs
+++ src/Arcus.Templates.WebApi/Program.cs
@@ -31,7 +31,7 @@ namespace Arcus.Templates.WebApi
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
-                .WriteTo.ApplicationInsights(instrumentationKey, new EventTelemetryConverter())
+                .WriteTo.ApplicationInsights(instrumentationKey, new TraceTelemetryConverter())
                 .CreateLogger();
 
             try

ffc0b1078730ff90cd5a9479350750c1506182f9
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2020-02-11 18:39:21 +0100

    PR-FIX: use 'utf-8' io 'ascii'

--- src/Arcus.Templates.WebApi/Startup.cs
+++ src/Arcus.Templates.WebApi/Startup.cs
@@ -100,7 +100,7 @@ namespace Arcus.Templates.WebApi
 #endif
             });
 #if JwtAuth
-            var key = Encoding.ASCII.GetBytes(Configuration["Jwt:Secret"]);
+            var key = Encoding.UTF8.GetBytes(Configuration["Jwt:Secret"]);
             services.AddAuthentication(x =>
                     {
                         x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;

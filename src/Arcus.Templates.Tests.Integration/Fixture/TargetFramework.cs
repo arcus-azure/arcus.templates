a9c03c1ab23c3f999d10acc6f51c6804fd8141a7
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2020-02-19 14:07:52 +0100

    CHORE: update worker template projects from .NET Core 3.0 > 3.1

--- src/Arcus.Templates.Tests.Integration/Fixture/TargetFramework.cs
+++ src/Arcus.Templates.Tests.Integration/Fixture/TargetFramework.cs
@@ -11,8 +11,8 @@
         NetCoreApp22,
 
         /// <summary>
-        /// Sets the target framework of the project created from the templates to netcoreapp3.0.
+        /// Sets the target framework of the project created from the templates to netcoreapp3.1.
         /// </summary>
-        NetCoreApp30
+        NetCoreApp31
     }
 }

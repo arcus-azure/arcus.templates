52f86b60f89258c6fbdaf7c908c926ac12a5b1a8
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-04-06 10:52:31 +0200

    Update src/Arcus.Templates.WebApi/Startup.cs
    Co-Authored-By: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Arcus.Templates.WebApi/Startup.cs
+++ src/Arcus.Templates.WebApi/Startup.cs
@@ -164,7 +164,7 @@ namespace Arcus.Templates.WebApi
 
 #if (ExcludeCorrelation == false)
                 swaggerGenerationOptions.OperationFilter<AddHeaderOperationFilter>("RequestId", "The header that will have an unique identifier to distinguish between requests/responses", false);
-                swaggerGenerationOptions.OperationFilter<AddHeaderOperationFilter>("X-Transaction-Id", "The header that will have an identifier that relate requests/responses", false);
+                swaggerGenerationOptions.OperationFilter<AddHeaderOperationFilter>("X-Transaction-Id", "Transaction ID is used to correlate multiple operation calls. A new transaction ID will be generated if not specified.", false);
                 swaggerGenerationOptions.OperationFilter<AddResponseHeadersFilter>();
 #endif
             });

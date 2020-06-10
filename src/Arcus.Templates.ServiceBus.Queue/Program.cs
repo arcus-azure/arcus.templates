c7d578211da8906988d606cefbfeab459d2693f5
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-06-10 10:41:38 +0200

    pr-sug: update with styling of closing if directive

--- src/Arcus.Templates.ServiceBus.Queue/Program.cs
+++ src/Arcus.Templates.ServiceBus.Queue/Program.cs
@@ -65,7 +65,7 @@ namespace Arcus.Templates.ServiceBus.Queue
                        {
 //[#if DEBUG]
                            stores.AddConfiguration(config);
-                           //[#endif]
+//[#endif]
 
                            //#error Please provide a valid secret provider, for example Azure Key Vault: https: //security.arcus-azure.net/features/secrets/consume-from-key-vault
                            stores.AddAzureKeyVaultWithManagedServiceIdentity("https://your-keyvault-vault.azure.net/");

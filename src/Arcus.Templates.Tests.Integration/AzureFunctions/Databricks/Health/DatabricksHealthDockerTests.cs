962773320e6f2bd841168de93776bfe2d1de94c7
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-11-03 18:25:58 +0100

    pr-style: remove empty space

--- src/Arcus.Templates.Tests.Integration/AzureFunctions/Databricks/Health/DatabricksHealthDockerTests.cs
+++ src/Arcus.Templates.Tests.Integration/AzureFunctions/Databricks/Health/DatabricksHealthDockerTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace Arcus.Templates.Tests.Integration.AzureFunctions.Databricks.Health
 {
@@ -10,6 +6,5 @@ namespace Arcus.Templates.Tests.Integration.AzureFunctions.Databricks.Health
     [Trait("Category", TestTraits.Docker)]
     public class DatabricksHealthDockerTests
     {
-
     }
 }

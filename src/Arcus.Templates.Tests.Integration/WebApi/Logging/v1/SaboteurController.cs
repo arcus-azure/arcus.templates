6d52210d0cc02711599a199b829e735786bee0c1
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-03-25 07:39:59 +0100

    fix: failed response request tracking (#327)

---
+++ src/Arcus.Templates.Tests.Integration/WebApi/Logging/v1/SaboteurController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arcus.Templates.Tests.Integration.WebApi.Logging.v1
+{
+    /// <summary>
+    /// Represents an API controller that sabotage the HTTP endpoint by throwing an exception.
+    /// </summary>
+    [Route("api/v1")]
+    [ApiController]
+    public class SaboteurController : ControllerBase
+    {
+        public const string Route = "sabotage";
+        
+        [HttpGet]
+        [Route(Route)]
+        public IActionResult Sabotage()
+        {
+            throw new Exception("Sabotage the HTTP endpoint by throwing an general exception");
+        }
+    }
+}

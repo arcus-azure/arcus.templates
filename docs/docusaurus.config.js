932682b307a2c3809bbd91d4c1f4c6b784b0361c
Michael Calewaert <michael.calewaert@codit.eu>
2021-10-09 09:47:30 +0200

    docs: Add search (#443)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,6 +12,15 @@ module.exports = {
   organizationName: 'arcus-azure', // Usually your GitHub org/user name.
   projectName: 'Arcus - Templates', // Usually your repo name.
   themeConfig: {
+    algolia: {
+      apiKey:process.env.ALGOLIA_API_KEY,
+      indexName: 'arcus-azure',
+      // Set `contextualSearch` to `true` when having multiple versions!!!
+      contextualSearch: true,
+      searchParameters: {
+        facetFilters: ["tags:templates"]
+      },
+    },
     image: 'img/arcus.jpg',
     navbar: {
       title: 'Templates',

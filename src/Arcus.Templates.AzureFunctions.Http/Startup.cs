6dbea1ec76a84f9f178ae3f250f4791b50afba30
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-04-13 12:02:07 +0200

    fix: use correct appinsights instrumentation key env var

--- src/Arcus.Templates.AzureFunctions.Http/Startup.cs
+++ src/Arcus.Templates.AzureFunctions.Http/Startup.cs
@@ -52,7 +52,7 @@ namespace Arcus.Templates.AzureFunctions.Http
                 stores.AddAzureKeyVaultWithManagedIdentity("https://your-keyvault.vault.azure.net/", CacheConfiguration.Default);
             });
 
-            var instrumentationKey = config.GetValue<string>("APPLICATIONINSIGHTS_INSTRUMENTATIONKEY");
+            var instrumentationKey = config.GetValue<string>("APPINSIGHTS_INSTRUMENTATIONKEY");
             var configuration = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
@@ -64,8 +64,7 @@ namespace Arcus.Templates.AzureFunctions.Http
 
             builder.Services.AddLogging(logging =>
             {
-                logging.ClearProvidersExceptFunctionProviders()
-                       .AddSerilog(configuration.CreateLogger(), dispose: true);
+                logging.AddSerilog(configuration.CreateLogger(), dispose: true);
             });
         }
     }

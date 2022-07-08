df00366a5cc9f9fedfdec7e27b34aae0a083849b
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-07-08 11:22:26 +0200

    test: add integration test for webapi w/ excluded exception in health report

--- src/Arcus.Templates.WebApi/Program.cs
+++ src/Arcus.Templates.WebApi/Program.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 #if Console
 using Microsoft.Extensions.Logging; 

6a3e4e6710df068c656ece973de1d3e5135c95b3
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-09-26 08:14:23 +0200

    pr-fix: update with removal of user errors

--- src/Arcus.Templates.Tests.Integration/AzureFunctions/Http/AzureFunctionsHttpProject.cs
+++ src/Arcus.Templates.Tests.Integration/AzureFunctions/Http/AzureFunctionsHttpProject.cs
@@ -184,7 +184,8 @@ namespace Arcus.Templates.Tests.Integration.AzureFunctions.Http
             var project = new AzureFunctionsHttpProject(configuration, outputWriter);
             project.CreateNewProject(options);
             project.AddLocalSettings(FunctionsWorker.InProcess);
-            
+            project.UpdateFileInProject("Startup.cs", contents => project.RemovesUserErrorsFromContents(contents));
+
             return project;
         }
 

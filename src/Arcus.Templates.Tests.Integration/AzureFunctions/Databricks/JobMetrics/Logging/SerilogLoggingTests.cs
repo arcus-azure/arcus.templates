aaa5c670b1142f848a60c654d3571f0f4e978eed
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-09-26 13:12:38 +0200

    feat: add exclude serilog project option to az func databricks jobmetrics project template

---
+++ src/Arcus.Templates.Tests.Integration/AzureFunctions/Databricks/JobMetrics/Logging/SerilogLoggingTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Arcus.Templates.Tests.Integration.Fixture;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Arcus.Templates.Tests.Integration.AzureFunctions.Databricks.JobMetrics.Logging
+{
+    [Collection(TestCollections.Integration)]
+    [Trait("Category", TestTraits.Integration)]
+    public class SerilogLoggingTests
+    {
+        private readonly ITestOutputHelper _outputWriter;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SerilogLoggingTests" /> class.
+        /// </summary>
+        public SerilogLoggingTests(ITestOutputHelper outputWriter)
+        {
+            _outputWriter = outputWriter;
+        }
+
+        [Fact]
+        public async Task DatabricksProject_WithoutSerilog_StillRuns()
+        {
+            // Arrange
+            var config = TestConfig.Create();
+            var options = new AzureFunctionsDatabricksProjectOptions().WithExcludeSerilog();
+
+            // Act
+            using (var project = AzureFunctionsDatabricksProject.StartNew(config, options, _outputWriter))
+            {
+                // Assert
+                await project.Admin.TriggerFunctionAsync();
+                Assert.DoesNotContain("Serilog", project.GetFileContentsOfProjectFile());
+                Assert.DoesNotContain("Serilog", project.GetFileContentsInProject("Startup.cs"));
+            }
+        }
+    }
+}

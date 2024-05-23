051636f31ce2ffe2fd8e4bfe0e3f1bb9def9eddd
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2024-05-23 09:27:46 +0200

    chore: upgrade templates to net8.0 (#873)
    * chore: upgrade templates to net8.0
    
    * pr-fix: use net8.0 in ci build
    
    * pr-fix: use diff dotnet import for docker tests
    
    * pr-fix: remove arcus evengrid from integration tests
    
    * pr-fix: remove arcus eventgrid packages implicit references
    
    * pr-fix: remove dotnet install in docker integration tests
    
    * pr-fix: use automatic topic subscription
    
    * pr-fix: update Dockerfiles
    
    * pr-revert: program file improvements
    
    * pr-revert: unnecessary blank space in program
    
    * pr-fix: remove pinned az cli tool
    
    * pr-temp: verify if the problem is timeout
    
    * pr-fix: remove eventgrid from servicebus test message hanlder
    
    * pr-fix: simplify worker projects w/o eventgrid references
    
    * pr-add: write log message upon message processing

---
+++ src/Arcus.Templates.Tests.Integration/Worker/ServiceBus/Fixture/WriteToFileMessageHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Arcus.Messaging.Abstractions;
+using Arcus.Messaging.Abstractions.ServiceBus;
+using Arcus.Messaging.Abstractions.ServiceBus.MessageHandling;
+using Microsoft.Extensions.Logging;
+
+namespace Arcus.Templates.Tests.Integration.Worker.ServiceBus.Fixture
+{
+    public class WriteToFileMessageHandler : IAzureServiceBusMessageHandler<Order>
+    {
+        private readonly ILogger<WriteToFileMessageHandler> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WriteToFileMessageHandler" /> class.
+        /// </summary>
+        public WriteToFileMessageHandler(ILogger<WriteToFileMessageHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task ProcessMessageAsync(
+            Order orderMessage,
+            AzureServiceBusMessageContext messageContext,
+            MessageCorrelationInfo correlationInfo,
+            CancellationToken cancellationToken)
+        {
+            var eventData = new OrderCreatedEventData(
+                orderMessage.Id,
+                orderMessage.Amount,
+                orderMessage.ArticleNumber,
+                $"{orderMessage.Customer.FirstName} {orderMessage.Customer.LastName}",
+                correlationInfo);
+
+            string json = JsonSerializer.Serialize(eventData);
+            string currentDirPath = Directory.GetCurrentDirectory();
+
+            var fileName = $"{correlationInfo.TransactionId}.json";
+            _logger.LogTrace("Processed message by writing on disk: {FileName}", fileName);
+            File.WriteAllText(Path.Combine(currentDirPath, fileName), json);
+
+            return Task.CompletedTask;
+        }
+    }
+}
